5725df9e1997ae60b7b759cad2a6d0c52c21cb94
Aphirak <aphirak06032548@gmail.com>
2026-08-08 15:57:54 +0700

    fix build script

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,11 +1,27 @@
 import Form from "@/components/Form";
+import { prisma } from "@/lib/prisma";
 
-const bookPage = () => {
+const bookPage = async () => {
+  const books = await prisma.book.findMany({ orderBy: { createdAt: "desc" } });
+
   return (
     <>
-      <h1>Book Page</h1>
-
       <Form />
+
+      <h1>รายการหนังสือ</h1>
+
+      {books.length === 0 ? (
+        <p>ไม่มีหนังสือ</p>
+      ) : (
+        <ul>
+          {books.map((book) => (
+            <li key={book.id}>
+              <p className="font-semibold">ชื่อหนังสือ: {book.title}</p>
+              <p>ราคา: {book.price.toLocaleString("th-TH")} บาท</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 };
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,6 +1,8 @@
 "use server";
 
-// export const createBook = async (_prevState, formData) => {
+import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+
 export const createBook = async (_prevState: string | null, formData: FormData) => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   console.log("formData", formData);
@@ -11,6 +13,15 @@ export const createBook = async (_prevState: string | null, formData: FormData) 
   if (!title || !Number.isInteger(price) || price <= 0) {
     return "กรุณากรอกข้อมูลให้ถูกต้อง";
   }
+
+  await prisma.book.create({
+    data: {
+      title,
+      price,
+    },
+  });
+
+  revalidatePath("/book");
 
   return "Book created successfully";
 };
